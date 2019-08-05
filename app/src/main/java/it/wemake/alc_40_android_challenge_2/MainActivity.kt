a4826d348d4c1088221be79e4489955502c3a29a
mohammed <mohammed>
2019-08-05 13:10:56 +0100

    Initial commit

---
+++ app/src/main/java/it/wemake/alc_40_android_challenge_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package it.wemake.alc_40_android_challenge_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
